7bb7d74dd650b73c99ddda7e44b4490b6cce0d14
Yeonnies <126739852+Yeonnies@users.noreply.github.com>
2025-09-12 00:07:45 +0900

    [Bronze V] Title: 시험 성적, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.swift
@@ -0,0 +1,13 @@
+let num = Int(readLine()!)!
+
+if num >= 90 {
+    print("A")
+} else if num >= 80 {
+    print("B")
+} else if num >= 70 {
+    print("C")
+} else if num >= 60 {
+    print("D")
+} else {
+    print("F")
+}
