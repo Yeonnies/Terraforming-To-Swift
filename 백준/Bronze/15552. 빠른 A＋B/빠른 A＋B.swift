1c2995c97a17edba09d60075a024b398237e63de
Yeonnies <126739852+Yeonnies@users.noreply.github.com>
2025-09-15 13:48:42 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 696 ms, Memory: 194564 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+let data = FileHandle.standardInput.readDataToEndOfFile()
+let input = String(data: data, encoding: .utf8)!
+
+let numbers = input.split { $0 == " " || $0 == "\n" }.map { Int($0)! }
+
+let t = numbers[0]
+
+var output = [String]()
+output.reserveCapacity(t)
+
+var index = 1
+for _ in 0..<t {
+    let a = numbers[index]
+    let b = numbers[index + 1]
+    
+    output.append("\(a + b)")
+    
+    index += 2
+}
+
+let result = output.joined(separator: "\n")
+FileHandle.standardOutput.write(result.data(using: .utf8)!)
